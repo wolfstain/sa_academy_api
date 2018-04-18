fc1432f0018973f0475090e75530200953790013
wolfstain <dahoyosr@unal.edu.co>
2018-04-18 12:18:26 +0000

    p2

--- src/graphQLSchema.js
+++ src/graphQLSchema.js
@@ -1,56 +1,67 @@
-import merge from 'lodash.merge';
-import GraphQLJSON from 'graphql-type-json';
-import { makeExecutableSchema } from 'graphql-tools';
+    import merge from 'lodash.merge';
+    import GraphQLJSON from 'graphql-type-json';
+    import { makeExecutableSchema } from 'graphql-tools';
 
-import { mergeSchemas } from './utilities';
+    import { mergeSchemas } from './utilities';
 
-import {
-	studentsMutations,
-	studentsQueries,
-	studentsTypeDef
-} from './students/typeDefs';
+    import {
+        studentsMutations,
+        studentsQueries,
+        studentsTypeDef
+    } from './students/typeDefs';
 
-import {
-	coursesMutations,
-	coursesQueries,
-	coursesTypeDef
-} from './courses/typeDefs';
+    import {
+        coursesMutations,
+        coursesQueries,
+        coursesTypeDef
+    } from './courses/typeDefs';
 
-import {
-	gradesMutations,
-	gradesQueries,
-	gradesTypeDef
-} from './grades/typeDefs';
+    import {
+        gradesMutations,
+        gradesQueries,
+        gradesTypeDef
+    } from './grades/typeDefs';
 
-import studentsResolvers from './students/resolvers';
-import coursesResolvers from './courses/resolvers';
-import gradesResolvers from './grades/resolvers';
+    import {
+        authMutations,
+        authTypeDef
+    } from './auth/typeDefs';
 
-// merge the typeDefs
-const mergedTypeDefs = mergeSchemas(
-	[
-		'scalar JSON',
-		studentsTypeDef,
-		coursesTypeDef,
-		gradesTypeDef
-	],
-	[
-		studentsQueries,
-		coursesQueries,
-		gradesQueries
-	],
-	[
-		studentsMutations,
-		coursesMutations,
-		gradesMutations
-	]
-);
+    import studentsResolvers from './students/resolvers';
+    import coursesResolvers from './courses/resolvers';
+    import gradesResolvers from './grades/resolvers';
+    import authResolvers from './auth/resolvers';
 
-// Generate the schema object from your types definition.
-export default makeExecutableSchema({
-	typeDefs: mergedTypeDefs,
-	resolvers: merge(
-		{ JSON: GraphQLJSON }, // allows scalar JSON
-		studentsResolvers
-	)
-});
+    // merge the typeDefs
+    const mergedTypeDefs = mergeSchemas(
+        [
+            'scalar JSON',
+            studentsTypeDef,
+            coursesTypeDef,
+            gradesTypeDef,
+            authTypeDef
+        ],
+        [
+            studentsQueries,
+            coursesQueries,
+            gradesQueries
+        ],
+        [
+            studentsMutations,
+            coursesMutations,
+            gradesMutations,
+            authMutations
+        ]
+    );
+
+    // Generate the schema object from your types definition.
+    export default makeExecutableSchema({
+        typeDefs: mergedTypeDefs,
+        resolvers: merge(
+            { JSON: GraphQLJSON }, // allows scalar JSON
+            studentsResolvers,
+            coursesResolvers,
+            gradesResolvers,
+            authResolvers
+        )
+    });
