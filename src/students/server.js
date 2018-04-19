3ac6982013be262260aa0688c8dc7524e797bcac
wolfstain <dahoyosr@unal.edu.co>
2018-04-19 03:48:08 +0000

    f

--- src/students/server.js
+++ src/students/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.STUDENTS_URL || '35.227.46.47';
+export const url = process.env.STUDENTS_URL || 'students-ms';
 export const port = process.env.STUDENTS_PORT || '4000';
 export const entryPoint = process.env.STUDENTS_ENTRY || 'students';
